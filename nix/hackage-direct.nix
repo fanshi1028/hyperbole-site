c78f8ec8bb908d494381cdcb77d59090af77fa6c
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2026-01-31 12:28:32 +0800

    fixup! try disable Hoogle

--- nix/hackage-direct.nix
+++ nix/hackage-direct.nix
@@ -44,69 +44,69 @@ hself: hsuper: {
     sha256 = "sha256-u3ida68KfdhGpHs4LiStuv49lsffxypf15dYYIHYcd4=";
   } { };
 
-  # tls = hself.callHackageDirect {
-  #   pkg = "tls";
-  #   ver = "2.2.1";
-  #   sha256 = "sha256-XrksEXOsZDF+jn/rI5m2F+fIu05zCPcO5CZRXFNoeJY=";
-  # } { };
+  tls = hself.callHackageDirect {
+    pkg = "tls";
+    ver = "2.2.1";
+    sha256 = "sha256-XrksEXOsZDF+jn/rI5m2F+fIu05zCPcO5CZRXFNoeJY=";
+  } { };
 
-  # crypton-asn1-encoding = hself.callHackageDirect {
-  #   pkg = "crypton-asn1-encoding";
-  #   ver = "0.10.0";
-  #   sha256 = "sha256-dTP26qiOVnAb5XO/gibuG1rYI03vDTpBr6+L79PsjEA=";
-  # } { };
+  crypton-asn1-encoding = hself.callHackageDirect {
+    pkg = "crypton-asn1-encoding";
+    ver = "0.10.0";
+    sha256 = "sha256-dTP26qiOVnAb5XO/gibuG1rYI03vDTpBr6+L79PsjEA=";
+  } { };
 
-  # crypton-asn1-types = hself.callHackageDirect {
-  #   pkg = "crypton-asn1-types";
-  #   ver = "0.4.1";
-  #   sha256 = "sha256-+mjsNBKhhxFrNmCCMLPsugzlz/61Glqw8hKoYndy+wc=";
-  # } { };
+  crypton-asn1-types = hself.callHackageDirect {
+    pkg = "crypton-asn1-types";
+    ver = "0.4.1";
+    sha256 = "sha256-+mjsNBKhhxFrNmCCMLPsugzlz/61Glqw8hKoYndy+wc=";
+  } { };
 
-  # time-hourglass = hself.callHackageDirect {
-  #   pkg = "time-hourglass";
-  #   ver = "0.3.0";
-  #   sha256 = "sha256-zVgLI6zt0FPFzRiey3wG6NLOT08ENDIweAUDyj0n1YU=";
-  # } { };
+  time-hourglass = hself.callHackageDirect {
+    pkg = "time-hourglass";
+    ver = "0.3.0";
+    sha256 = "sha256-zVgLI6zt0FPFzRiey3wG6NLOT08ENDIweAUDyj0n1YU=";
+  } { };
 
-  # base16 = hself.callHackageDirect {
-  #   pkg = "base16";
-  #   ver = "1.0";
-  #   sha256 = "sha256-pLnipLnF7YuQvCwgw7Lp7sbwhab63sdEpubeSpaoEmY=";
-  #   rev = {
-  #     revision = "4";
-  #     sha256 = "sha256-TdNBIoFo8s3gOVTdchde5yagcT4iAPbHtz9M2NIgc3c=";
-  #   };
-  # } { };
+  base16 = hself.callHackageDirect {
+    pkg = "base16";
+    ver = "1.0";
+    sha256 = "sha256-pLnipLnF7YuQvCwgw7Lp7sbwhab63sdEpubeSpaoEmY=";
+    rev = {
+      revision = "4";
+      sha256 = "sha256-TdNBIoFo8s3gOVTdchde5yagcT4iAPbHtz9M2NIgc3c=";
+    };
+  } { };
 
-  # crypton-x509 = hself.callHackageDirect {
-  #   pkg = "crypton-x509";
-  #   ver = "1.8.0";
-  #   sha256 = "sha256-wxU8Ou52UCuCT2gbxqPKssteIVGUyg5WEbv1xRIyZTg=";
-  # } { };
+  crypton-x509 = hself.callHackageDirect {
+    pkg = "crypton-x509";
+    ver = "1.8.0";
+    sha256 = "sha256-wxU8Ou52UCuCT2gbxqPKssteIVGUyg5WEbv1xRIyZTg=";
+  } { };
 
-  # crypton-x509-store = hself.callHackageDirect {
-  #   pkg = "crypton-x509-store";
-  #   ver = "1.8.0";
-  #   sha256 = "sha256-U6DH5Ke3JXAzZuqxLM6mPKDxqj4HTf5kjoBXaerLOcc=";
-  # } { };
+  crypton-x509-store = hself.callHackageDirect {
+    pkg = "crypton-x509-store";
+    ver = "1.8.0";
+    sha256 = "sha256-U6DH5Ke3JXAzZuqxLM6mPKDxqj4HTf5kjoBXaerLOcc=";
+  } { };
 
-  # crypton-x509-validation = hself.callHackageDirect {
-  #   pkg = "crypton-x509-validation";
-  #   ver = "1.8.0";
-  #   sha256 = "sha256-CyRqTUOcUzzVlQfTd3yylwDVtOaumBbBg9hMyvtcu7c=";
-  # } { };
+  crypton-x509-validation = hself.callHackageDirect {
+    pkg = "crypton-x509-validation";
+    ver = "1.8.0";
+    sha256 = "sha256-CyRqTUOcUzzVlQfTd3yylwDVtOaumBbBg9hMyvtcu7c=";
+  } { };
 
-  # crypton-pem = hself.callHackageDirect {
-  #   pkg = "crypton-pem";
-  #   ver = "0.3.0";
-  #   sha256 = "sha256-RBQdPqN/UJw+9FU/HAh5wR5S69WOfllnWAs/mpegbK8=";
-  # } { };
+  crypton-pem = hself.callHackageDirect {
+    pkg = "crypton-pem";
+    ver = "0.3.0";
+    sha256 = "sha256-RBQdPqN/UJw+9FU/HAh5wR5S69WOfllnWAs/mpegbK8=";
+  } { };
 
-  # crypton-asn1-parse = hself.callHackageDirect {
-  #   pkg = "crypton-asn1-parse";
-  #   ver = "0.10.0";
-  #   sha256 = "sha256-awY6Rk3LBgOZk9xEKGlPWtYYD8OiKAskN26otBbVXjc=";
-  # } { };
+  crypton-asn1-parse = hself.callHackageDirect {
+    pkg = "crypton-asn1-parse";
+    ver = "0.10.0";
+    sha256 = "sha256-awY6Rk3LBgOZk9xEKGlPWtYYD8OiKAskN26otBbVXjc=";
+  } { };
 
   # crypton-connection = hself.callHackageDirect {
   #   pkg = "crypton-connection";
